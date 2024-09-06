596e077c3b6a46a6e858996d6bd8f0aa867dbc4f
jason <originalkimchi100@gmail.com>
2024-09-06 09:02:02 +0900

    shader_prac

--- lib/math_shader.dart
+++ lib/math_shader.dart
@@ -61,8 +61,8 @@ class _MathShaderWidgetState extends State<MathShaderWidget> with SingleTickerPr
               alignment: Alignment.center,
               child: Container(
                 child: SizedBox(
-                  width: 700,
-                  height:700,
+                  width: 800,
+                  height:800,
                   child: ShaderBuilder(
                     assetKey: 'assets/shaders/circle_shader.frag',
                         (BuildContext context, FragmentShader shader, _) => CustomPaint(
